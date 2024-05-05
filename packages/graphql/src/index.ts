b27bcaaae228fe86f42758b6302025c634ab0a0a
Eduardo Salazar <eduardosalazar89@hotmail.es>
2024-05-04 20:01:01 -0600

    fix: export graphql format exception helper

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -1,4 +1,5 @@
 export * from './dto/pagination-params.input';
 export * from './entities/pagination.entity';
+export * from './helpers/format-exception.helper';
 export * from './resolvers/app-info-resolver.resolver';
 export * from './scalars/Upload';
